795ad9204a669bc5e06e130a80c96a3b923e575d
vincelinyc <hi@vince.li>
2017-09-11 19:37:02 -0400

    Small update to the grid

--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
-console.log('hello world');
+$("#moreAboutMe").click(function() {
+    document.getElementById("aboutMe").innerHTML = "I'm Vince Li, a Product Designer from Amsterdam living in NYC. Currently building internal data tools at Oracle. Previously freelancing for startups like Pager, Maslow (rip) and Con Amore.<br><br>Before that I founded Schmeck Pony (rip). Warehouse parties in Amsterdam for which I build this memorial site.";
+});
